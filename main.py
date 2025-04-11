401d596ff4bd268406d8e1c7e5b681594422db45
Arthur L. <124107840+webarthu@users.noreply.github.com>
2025-04-11 18:29:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from crud import adicionar_usuario, listar_usuarios, atualizar_usuario, deletar_
 
 criar_tabelas()
 
-# Exemplos de uso:
 adicionar_usuario("Arthur", "arthur@email.com")
 adicionar_usuario("Maria", "maria@email.com")
 
